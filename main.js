45c505f6de51f9350904724e8da4bc879b85aa80
Ruth Delgado <rinesdelgo@gmail.com>
2024-02-07 19:10:48 -0300

    cambios-que-hice-ayer

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
-const balance = document.getElementById("Balance");
-const reportes = document.getElementById("reportes");
-const categorias = document.getElementById("categorias");
-const nuevaOperacion = document.getElementById("nuevaOperacion");
-const aBalance = document.querySelector(".balance1");
-const aReportes = document.querySelector(".reportes1");
-const aCategorias = document.querySelector(".categorias1");
+const $ = (selector) => document.querySelector(selector)
 
-aReportes.addEventListener("click", () => {
-  reportes.classList.remove("hidden");
-  balance.classList.add("hidden");
-  categorias.classList.add("hidden");
-});
+// const balance = document.getElementById("Balance");
+// const reportes = document.getElementById("reportes");
+// const categorias = document.getElementById("categorias");
+// const nuevaOperacion = document.getElementById("nuevaOperacion");
+// const aBalance = document.querySelector(".balance1");
+// const aReportes = document.querySelector(".reportes1");
+// const aCategorias = document.querySelector(".categorias1");
 
-aBalance.addEventListener("click", () => {
-  balance.classList.remove("hidden");
-  categorias.classList.add("hidden");
-  reportes.classList.add("hidden");
-});
+$("#navbar-responsive").addEventListener("click", () =>{
+  $("#mobile-menu").classList.toggle("visible")
+  $("#mobile-menu").classList.toggle("hidden")
+})
 
-aCategorias.addEventListener("click", () => {
-  categorias.classList.remove("hidden");
-  balance.classList.add("hidden");
-  reportes.classList.add("hidden");
-});
+$("#reportes").addEventListener("click", () => {
+  $("#reportes").classList.remove("hidden")
+  $("#reportes").classList.add("visible")
+  $("#Balance").classList.add("hidden")
+  $("#categorias").classList.add("hidden")
+})
+
+$("#categorias").addEventListener("click", () => {
+  $("#categorias").classList.remove("hidden")
+  $("#Balance").classList.add("hidden")
+  $("#reportes").classList.add("hidden")
+})
+
+$("#Balance").addEventListener("click", () => {
+  $("#Balance").classList.remove("hidden")
+  $("#reportes").classList.add("hidden")
+  $("#categorias").classList.add("hidden")
+})
+
+$("#nuevaOperacion").addEventListener("click", () =>{
+   $("#nuevaOperacion").classList.remove("hidden")
+   $("#Balance").classList.add("hidden")
+   $("#reportes").classList.add("hidden")
+   $("#categorias").classList.add("hidden")
+})
+
+
+
